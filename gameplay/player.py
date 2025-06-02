808102d0d84c5989ff575874804f7b1a926b789f
gnd <peter@ftmo.com>
2025-06-02 21:13:10 +0200

    feat: adding new version of gameplay

--- gameplay/player.py
+++ gameplay/player.py
@@ -7,7 +7,6 @@ from screeninfo import get_monitors
 
 # Configuration
 REEL_WIDTH = 600
-REEL_ANOMALY = 100
 REELS_FOLDER = 'gameplay'
 PORT = 6666
 HOST = 'localhost'
@@ -20,6 +19,8 @@ process_list = []
 
 reel_files = [f for f in os.listdir(REELS_FOLDER) if (f.endswith('sludge.mp4') and f)]
 reel_count = len(reel_files)
+reel_files.sort()
+print(reel_files)
 
 if reel_count == 0:
     print("No reels found in folder.")
@@ -31,13 +32,18 @@ screen_width, screen_height = monitor.width, monitor.height
 current_reel_index = 0
 main_vid_fullscreen = True
 
+# Video dimensions
+width_half = int(screen_width / 2)
+width_smaller = int(screen_width / 2.66666)
+height_half = int(screen_height / 2)
+
 def launch_mainvid(file_path):
-    window_title = f"mplayer_0"
+    window_title = f"mplayer_main"
     
     cmd = [
         "mplayer",
         "-fs",
-        "-xy", "1920,1080",
+        "-xy", f"{screen_width},{screen_height}",
         "-vo", "x11",
         "-loop", "0",
         "-really-quiet",
@@ -64,37 +70,23 @@ def launch_mainvid(file_path):
 
     process_list.append(proc)
 
-def launch_player(file_path, index):
-    actual_reel_width = REEL_WIDTH
-
-    # open first 3 videos at predetrmined positions
-    match current_reel_index:
-        case 1:
-            pos_x = 0
-            pos_y = 780
-        case _:
-            pos_x = random.randint(0, max(0, screen_width - actual_reel_width))
-            pos_y = random.randint(0, max(0, screen_height - 500))
-    
+def launch_sidevid(file_path, index, x, y, width, height):
     window_title = f"mplayer_{index}"
 
     cmd = [
         "nice", "-n", "19",
         "mplayer",
-        "-xy", str(actual_reel_width),
-        "-geometry", f"{pos_x}:{pos_y}",
+        "-xy", f"{width},{height}",
+        "-geometry", f"{x}:{y}",
         "-loop", "0",
         "-really-quiet",
         "-noborder",
         "-nomouseinput",
-        # some stuff to speed up
-        #"-vo", "xv",
         "-hardframedrop",
         "-double",
         "-nosub",
         # more quiet
         "-af", f"volume=-{LOUD}",
-        "-aspect", "4:3",
         "-title",
         window_title,
         file_path
@@ -128,7 +120,6 @@ def move_window(title, x, y, width, height):
     Uses wmctrl to move and resize a window by title.
     """
     try:
-        
         # Then make it a bit smaller to make place for other videos
         wmctrl_cmd = ["wmctrl", "-r", title, "-e", f"0,{x},{y},{width},{height}"]
         #print(wmctrl_cmd)
@@ -168,23 +159,34 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             command = data.decode().strip()
             if command == 'new':
                 print("Launching new process.")
-                current_reel_index = (current_reel_index + 1) % reel_count
-                match current_reel_index:
+                match len(process_list):
+                    # Second video layer
                     case 1:
-                        toggle_fullscreen("mplayer_0", main_vid_fullscreen)
-                        main_vid_fullscreen = not main_vid_fullscreen
-                        move_window("mplayer_0", 0, 0, 1920, 780)
-                        launch_player(os.path.join(REELS_FOLDER, reel_files[current_reel_index]), current_reel_index)
-                        print(f"{len(process_list)} processes running..")                
-                    case _:
-                        launch_player(os.path.join(REELS_FOLDER, reel_files[current_reel_index]), current_reel_index)
+                        launch_sidevid(os.path.join(REELS_FOLDER, reel_files[current_reel_index]), current_reel_index, 0, 0, width_smaller, screen_height)
                         print(f"{len(process_list)} processes running..")
+                    # Third video layer
+                    case 2:
+                        toggle_fullscreen("mplayer_main", main_vid_fullscreen)
+                        main_vid_fullscreen = not main_vid_fullscreen
+                        move_window("mplayer_main", width_half, 0, width_half, 1080)
+                        move_window("mplayer_0", 0, 0, width_half, height_half)
+                        # launch third video
+                        launch_sidevid(os.path.join(REELS_FOLDER, reel_files[current_reel_index]), current_reel_index, 0, height_half, width_half, height_half)
+                        print(f"{len(process_list)} processes running..")
+                    # All others
+                    case _:
+                        pos_x = random.randint(0, max(0, screen_width - REEL_WIDTH))
+                        pos_y = random.randint(0, max(0, screen_height - 500))
+                        launch_sidevid(os.path.join(REELS_FOLDER, reel_files[current_reel_index]), current_reel_index, pos_x, pos_y, REEL_WIDTH, 0)
+                        print(f"{len(process_list)} processes running..")
+                # increase reel index
+                current_reel_index = (current_reel_index + 1) % reel_count
             if command == 'kill' and process_list:
                 kill_player(process_list.pop())
                 if (len(process_list) == 1):
-                    toggle_fullscreen("mplayer_0", main_vid_fullscreen)
+                    toggle_fullscreen("mplayer_main", main_vid_fullscreen)
                     main_vid_fullscreen = not main_vid_fullscreen
-                    move_window("mplayer_0", 0, 0, 1920, 1080)
+                    move_window("mplayer_main", 0, 0, screen_width, screen_height)
                 print("Killing last process.")
                 print(f"{len(process_list)} processes left..")
             if command == 'killall':
